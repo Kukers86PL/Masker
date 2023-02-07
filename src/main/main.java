0a3cdf62b12125b72496db0874fb46be7b4bd627
Lukasz Grzelak <lukasz.grzelak.86@gmail.com>
2023-02-07 08:38:06 +0100

    Version added

--- src/main/main.java
+++ src/main/main.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 
 public class main {
 
+	public final static String g_sVersion = "1.0";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame frame = new JFrame("My First GUI");
+		JFrame frame = new JFrame("Masker " + g_sVersion);
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    frame.setSize(300,300);
 	    JButton button = new JButton("Hello World");
