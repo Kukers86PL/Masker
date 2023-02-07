34b36af526e6c465d70312ed7a61cf6b3c62b5a0
Lukasz Grzelak <lukasz.grzelak.86@gmail.com>
2023-02-07 10:52:19 +0100

    OnChange implementation

--- src/main/main.java
+++ src/main/main.java
@@ -2,28 +2,69 @@ package main;
 
 import javax.swing.*;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class main {
 
-	public final static String g_sVersion = "1.0";
+	public static final String g_sVersion = "1.0";
+	
+	public static JFrame g_cFrame = new JFrame("Masker " + g_sVersion);
+	public static JPanel g_cPanelMain = new JPanel();
+	public static JPanel g_cPanelSecond = new JPanel();
+	public static JPasswordField g_cPassword = new JPasswordField(20);
+	public static int g_iCache = 0;
+	public static List<JButton> g_lcButtons = new ArrayList<JButton>();
+	public static Timer g_cTimer = new Timer();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		JFrame frame = new JFrame("Masker " + g_sVersion);
-	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    frame.setSize(400,80);
+		
+	    g_cFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    g_cFrame.setSize(400,80);
 	    
 	    JLabel label = new JLabel("Password:");
-	    frame.getContentPane().add(label);
+	    g_cPanelMain.add(label);
 	    
-	    JPasswordField password = new JPasswordField(20);
-	    frame.getContentPane().add(password);
+	    g_cPanelMain.add(g_cPassword);
 	    
 	    JButton button = new JButton("Copy");
-	    frame.getContentPane().add(button);
+	    g_cPanelMain.add(button);
 	    
-	    frame.setLayout(new FlowLayout());
-	    frame.setVisible(true);
+	    g_cFrame.add(g_cPanelMain);
+	    g_cFrame.add(g_cPanelSecond);
+	    
+	    g_cFrame.setLayout(new FlowLayout());
+	    g_cFrame.setVisible(true);
+	    
+	    g_cFrame.repaint();
+	    
+	    g_cTimer.scheduleAtFixedRate(new TimerTask() {
+	    	@Override
+	    	public void run() {
+	    		if (g_iCache != g_cPassword.getPassword().length)
+	    		{
+	    			g_iCache = g_cPassword.getPassword().length;
+	    			
+		    		g_cFrame.setSize(400,80);
+		  			g_cPanelSecond.removeAll();
+		  			g_lcButtons.clear();
+		  			
+		  			for (int i = 0; i < g_cPassword.getPassword().length; ++i)
+		  			{
+		  				JButton button = new JButton(String.valueOf(i + 1)); 
+		  				g_lcButtons.add(button);
+		  				g_cPanelSecond.add(button);
+		  				g_cFrame.setSize(400,120);
+		  			}
+		  			
+		  			g_cFrame.repaint();
+	    		}
+	    	}
+	    }, 1, 1);
 	}
-
 }
