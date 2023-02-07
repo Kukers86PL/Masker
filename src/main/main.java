cea095d27964c51958dccb17e586e884f6549c39
Lukasz Grzelak <lukasz.grzelak.86@gmail.com>
2023-02-07 08:34:36 +0100

    Hello World GUI

--- src/main/main.java
+++ src/main/main.java
@@ -1,10 +1,17 @@
 package main;
 
+import javax.swing.*;
+
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		JFrame frame = new JFrame("My First GUI");
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setSize(300,300);
+	    JButton button = new JButton("Hello World");
+	    frame.getContentPane().add(button); // Adds Button to content pane of frame
+	    frame.setVisible(true);
 	}
 
 }
--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author kuki
- *
- */
-module Remusk {
-}
